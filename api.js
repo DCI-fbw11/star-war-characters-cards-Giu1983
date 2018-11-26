edf3e8e4bd1ac235a2b111d5e36170615b5a63b5
Giu1983 <42543829+Giu1983@users.noreply.github.com>
2018-11-26 10:19:04 +0100

    Add files via upload

---
+++ api.js
@@ -0,0 +1,45 @@
+//Load Star Wars character
+document.getElementById('button').addEventListener('click', loadCharacter);
+
+function loadCharacter() {
+    var xhr = new XMLHttpRequest(); 
+    xhr.open('GET', 'https://swapi.co/api/people', true);
+
+    xhr.onload = function(){
+        if(this.status == 200){
+            var data = JSON.parse(this.responseText);
+            console.log(data.results);
+            handleData(data.results);
+        }
+        else {
+            reject (this.status = 404);  
+        }
+        
+    }
+    xhr.send();
+   
+}
+
+
+
+
+function handleData(data) {
+    document.getElementById("demo").innerHTML = ""
+    data.forEach(item => {
+      // console.log(item.name);
+      // let liItem = document.createElement("li");
+      //liItem.textContent = item.name;
+      //document.getElementById("demo").appendChild(liItem);
+      let str = `<div class='card-container'> 
+      <h3> name: ${item.name}</h3>
+    <h5> homeworld : ${item.homeworld}</h5>
+    <h5> height : ${item.height}</h5>
+    <h5>hair_color: ${item.hair_color}</h5>
+    </div>`;
+      document.getElementById("demo").innerHTML += str;
+    }); 
+  }
+
+
+
+  
